88722492b347f0c79e21031d39b9698c522ba163
Feminnakwe5 <feminnakwe5@gmail.com>
2023-02-18 11:56:33 -0500

    plan cards

--- src/components/Mainsection display/Selectplan.js
+++ src/components/Mainsection display/Selectplan.js
@@ -1,50 +1,18 @@
 import React from 'react';
-import arcade from '../images/arcade.png';
+import PlanCards from './PlanCards';
 
 class SelectPlan extends React.Component {
-  constructor() {
-    super();
-    this.cards = [
-      {
-        image: { arcade },
-        alt: 'arcade logo',
-        title: 'Arcade',
-        monthPrice: 9,
-        yearPrice: 90,
-        yearDiscount: '2 months free',
-      },
-      {
-        image: '../images/advanced.png',
-        alt: 'advanced logo',
-        title: 'Advanced',
-        monthPrice: 12,
-        yearPrice: 120,
-        yearDiscount: '2 months free',
-      },
-      {
-        image: '../images/pro.png',
-        alt: 'pro logo',
-        title: 'pro',
-        monthPrice: 15,
-        yearPrice: 150,
-        yearDiscount: '2 months free',
-      },
-    ];
-  }
-
   render() {
-    return this.cards.map((card) => {
-      return (
-        <div className='card-container'>
-          <div className='card'>
-            <img src={card.image} alt={card.alt} />
-            <h3>{card.title}</h3>
-            <p>${card.monthPrice}/mo</p>
-            <p>{card.yearDiscount}</p>
-          </div>
+    return (
+      <div>
+        <h1>Select your plan</h1>
+        <h2>you have the option of monthly or yearly</h2>
+        <PlanCards />
+        <div>
+          monthly <button> toggle </button> yearly
         </div>
-      );
-    });
+      </div>
+    );
   }
 }
 
