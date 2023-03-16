c7f7d777db39536237cd9c88c4cd27a2a366dc93
Feminnakwe5 <feminnakwe5@gmail.com>
2023-03-16 14:29:29 -0400

    functional component

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,12 +22,20 @@ class Main extends React.Component {
           />
         </div>
         <div className='main-content-btn'>
-          {this.props.stateCount > 1 && (
-            <button onClick={this.props.subtractCount} className='go-back-btn'>
-              Go Back
-            </button>
-          )}
-          <button onClick={this.props.addCount} className='next-step-btn'>
+          <button
+            onClick={this.props.subtractCount}
+            className={this.props.stateCount === 1 ? 'hidden' : 'go-back-btn'}
+          >
+            Go Back
+          </button>
+
+          <button
+            onClick={this.props.addCount}
+            className='next-step-btn'
+            type='submit'
+            form='form1'
+            disabled={this.props.formSubmit}
+          >
             {this.props.stateCount === 4 ? 'Confirm' : 'Next Step'}
           </button>
         </div>
